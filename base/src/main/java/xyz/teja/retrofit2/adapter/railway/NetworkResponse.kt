b109a6ec1e5976ee020c4efee5ade0ec10bf6563
Teja Konjeti <contact@teja.xyz>
2020-09-22 05:29:20 +0530

    - Updated documentation for Network Response  - Added JaCoCo test report run configuration

--- base/src/main/java/xyz/teja/retrofit2/adapter/railway/NetworkResponse.kt
+++ base/src/main/java/xyz/teja/retrofit2/adapter/railway/NetworkResponse.kt
@@ -10,18 +10,18 @@ package xyz.teja.retrofit2.adapter.railway
 /**
  * Represents the result of making a network request.
  *
- * @param T success body type for 2xx response.
- * @param U error body type for non-2xx response.
+ * @param T success body type.
+ * @param U error body type.
  */
 sealed class NetworkResponse<out T : Any, out U : Any> {
 
     /**
-     * A request that resulted in a response with a 2xx status code that has a body.
+     * A request that resulted in a successful response of type T.
      */
     data class Success<T : Any>(val body: T?) : NetworkResponse<T, Nothing>()
 
     /**
-     * A request that resulted in a response with a non-2xx status code.
+     * A request that resulted in a response of type U.
      */
     data class ServerError<U : Any>(val body: U?, val code: Int) : NetworkResponse<Nothing, U>()
 
